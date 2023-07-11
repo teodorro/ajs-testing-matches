5d63282190cf53bbe7a5f7a584b45e1db48a01ac
theo <edem.teo@gmail.com>
2023-07-12 00:39:28 +0300

    task

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,43 @@
-import sum from '../basic';
+import sortHealths from '../basic';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+test('should sort', () => {
+  const items = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  sortHealths(items);
 
-  expect(result).toBe(6);
+  expect(items).toEqual(expected);
+});
+
+test('should process null arg', () => {
+  const items = null;
+  const expected = null;
+  sortHealths(items);
+  expect(items).toEqual(expected);
+});
+
+test('should throw wrong health type', () => {
+  const items = [
+    { name: 'мечник', health: 'asd' },
+    { name: 'маг', health: null },
+    { name: 'лучник', health: 80 },
+  ];
+  expect(() => sortHealths(items)).toThrow();
+});
+
+test('should throw wrong type', () => {
+  const items = [
+    { name: 'мечник', health: 123 },
+    { name: 'asdf' },
+    null,
+  ];
+
+  expect(() => sortHealths(items)).toThrow('illegal argument');
 });
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,7 +1,15 @@
-export default function sum(items) {
-  let result = 0;
-  for (const item of items) {
-    result += item;
+export default function sortHealths(items) {
+  if (items == null) {
+    return items;
   }
-  return result;
+  items.sort((item1, item2) => {
+    if (item1 == null || item1.health == null || Number.isNaN(+item1.health)) {
+      throw Error('illegal argument');
+    }
+    if (item2 == null || item2.health == null || Number.isNaN(+item2.health)) {
+      throw Error('illegal argument');
+    }
+    return item2.health - item1.health;
+  });
+  return items;
 }
