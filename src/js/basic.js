c074ef2b7ab93171dc9203d4526d3524514c6edc
theo <edem.teo@gmail.com>
2023-07-12 14:24:13 +0300

    task & tests

--- src/js/basic.js
+++ src/js/basic.js
@@ -3,10 +3,8 @@ export default function sortHealths(items) {
     return items;
   }
   items.sort((item1, item2) => {
-    if (item1 == null || item1.health == null || Number.isNaN(+item1.health)) {
-      throw Error('illegal argument');
-    }
-    if (item2 == null || item2.health == null || Number.isNaN(+item2.health)) {
+    if (item1 == null || item1.health == null || Number.isNaN(+item1.health)
+    || item2 == null || item2.health == null || Number.isNaN(+item2.health)) {
       throw Error('illegal argument');
     }
     return item2.health - item1.health;
